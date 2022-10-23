4d5c0469be8e4131c43b1bdcdfc9597e2a12eb3d
dheida1 <dinaheidar13@gmail.com>
2022-10-23 17:54:14 -0500

    sample mvc client

---
+++ samples/MvcClient/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcClient.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
